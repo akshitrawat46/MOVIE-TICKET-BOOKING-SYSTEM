37fcc1a49f86ba45272e1ef7bed395df9a3ae20c
akshitrawat46 <akshitrawat454@gmail.com>
2026-09-13 20:43:22 +0530

    Keep Payment as one class per file

--- Payment.cpp
+++ Payment.cpp
@@ -1,38 +1,10 @@
-#include <iostream>
 #include <string>
 using namespace std;
 
-// Abstraction + inheritance + runtime polymorphism.
+// Abstraction: common payment contract.
 class Payment {
 public:
     virtual ~Payment() = default;
     virtual bool pay(double amount) = 0;
     virtual string getMethodName() const = 0;
-};
-
-class UpiPayment : public Payment {
-public:
-    bool pay(double amount) override {
-        cout << "Processing UPI payment of Rs." << amount << "...\n";
-        return true;
-    }
-    string getMethodName() const override { return "UPI"; }
-};
-
-class CardPayment : public Payment {
-public:
-    bool pay(double amount) override {
-        cout << "Processing Card payment of Rs." << amount << "...\n";
-        return true;
-    }
-    string getMethodName() const override { return "CARD"; }
-};
-
-class CashPayment : public Payment {
-public:
-    bool pay(double amount) override {
-        cout << "Cash payment received: Rs." << amount << "\n";
-        return true;
-    }
-    string getMethodName() const override { return "CASH"; }
 };
